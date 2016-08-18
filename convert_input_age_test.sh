af3b45fe10193304c87a5f159a2f7525ffbf8b14
Greg Day <gday@wayfair.com>
2016-08-18 12:02:11 -0400

    more updates to prockiller, rewrote main function

--- convert_input_age_test.sh
+++ convert_input_age_test.sh
@@ -5,6 +5,7 @@ function convert_input_age() {
     local day="86400"
     local hour="3600"
     local min="60"
+    local second="1"
     local age_in="$1"
     local len=${#age_in}
     case "$age_in" in
@@ -12,10 +13,13 @@ function convert_input_age() {
         *d) echo $((${age_in:0:$len - 1} * $day)) ;;
         *h) echo $((${age_in:0:$len - 1} * $hour)) ;;
         *m) echo $((${age_in:0:$len - 1} * $min));;
+        *s) echo $((${age_in:0:$len - 1} * $min));;
         *) echo $age_in ;;
     esac
+    return 0
 }
 
 MAXAGE_SECONDS=$(convert_input_age "$MAXAGE")
+echo $?
 echo $MAXAGE_SECONDS
 exit 0
