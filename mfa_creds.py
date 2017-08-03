d91b95907a727607a30b5151c9f144f380c69c1d
Greg Day <gday@cryptic.li>
2017-08-03 09:43:57 -0400

    Pep8 changes, break out function to _savecfg

--- mfa_creds.py
+++ mfa_creds.py
@@ -3,18 +3,31 @@
 # configured, and that you have MFA enabled for your IAM user
 
 import argparse
-import ConfigParser
+import json
 import os
 import subprocess
 import sys
-import json
+import ConfigParser
+
 from dateutil.parser import parse
+
+def _savecfg(config_file, parser):
+
+    try:
+        with open(config_file, 'wb') as cfg:
+            parser.write(cfg)
+    except EnvironmentError:
+        sys.stdout.write('       [\033[91mERROR\033[0m]\n')
+        sys.exit(1)
 
 def _getcreds(token, duration):
 
-    # Make sure ~/.aws/credentials exists
+    # Set config file
     home_dir = os.path.expanduser('~')
-    if not os.path.isfile(os.path.join(home_dir, '.aws', 'credentials')):
+    config_file = os.path.join(home_dir, '.aws', 'credentials')
+    # Make sure ~/.aws/credentials exists
+
+    if not os.path.isfile(config_file):
         sys.stderr.write('Error: File ~/.aws/credentials does not exist\n')
         sys.exit(1)
 
@@ -23,7 +36,7 @@ def _getcreds(token, duration):
     sys.stdout.flush()
 
     parser = ConfigParser.SafeConfigParser()
-    parser.read(os.path.join(home_dir, '.aws', 'credentials'))
+    parser.read(config_file)
 
     sys.stdout.write('                     [\033[92mOK\033[0m]\n')
     sys.stdout.write('Writing default access key to/from my-keys section...')
@@ -31,20 +44,27 @@ def _getcreds(token, duration):
 
     # Install default key. First run, will copy default key to my-keys instead
     try:
-        parser.set('default', 'aws_access_key_id', parser.get('my-keys', 'aws_access_key_id'))
-        parser.set('default', 'aws_secret_access_key', parser.get('my-keys', 'aws_secret_access_key'))
+        parser.set('default',
+                   'aws_access_key_id',
+                   parser.get('my-keys', 'aws_access_key_id'))
+        parser.set('default',
+                   'aws_secret_access_key',
+                   parser.get('my-keys', 'aws_secret_access_key'))
         parser.remove_option('default', 'aws_security_token')
     except ConfigParser.NoSectionError:
         parser.add_section('my-keys')
-        parser.set('my-keys', 'aws_access_key_id', parser.get('default', 'aws_access_key_id'))
-        parser.set('my-keys', 'aws_secret_access_key', parser.get('default', 'aws_secret_access_key'))
+        parser.set('my-keys',
+                   'aws_access_key_id',
+                   parser.get('default', 'aws_access_key_id'))
+        parser.set('my-keys',
+                   'aws_secret_access_key',
+                   parser.get('default', 'aws_secret_access_key'))
 
-    with open(os.path.join(home_dir, '.aws', 'credentials'), 'wb') as configfile:
-        parser.write(configfile)
+    _savecfg(config_file, parser)
 
     sys.stdout.write('      [\033[92mOK\033[0m]\n')
-
     sys.stdout.write('Looking up user and account info with AWS CLI...')
+
     stdout, stderr = subprocess.Popen(
         [
             'aws',
@@ -58,7 +78,7 @@ def _getcreds(token, duration):
     try:
         cli_output = json.loads(stdout)
     except ValueError:
-        sys.stdout.write('                     [\033[91mERROR\033[0m]\n')
+        sys.stdout.write('           [\033[91mERROR\033[0m]\n')
         sys.exit(1)
 
     sys.stdout.write('           [\033[92mOK\033[0m]\n')
@@ -89,30 +109,38 @@ def _getcreds(token, duration):
         sys.exit(1)
 
     sys.stdout.write('                     [\033[92mOK\033[0m]\n')
-    sys.stdout.write('Updating the ~/.aws/credentials file...')
+    sys.stdout.write('Saving final creds in the ~/.aws/credentials file... ')
     sys.stdout.flush()
 
     # Install the new credentials
-    parser.set('default', 'aws_access_key_id', mfa_session['Credentials']['AccessKeyId'])
-    parser.set('default', 'aws_secret_access_key', mfa_session['Credentials']['SecretAccessKey'])
-    parser.set('default', 'aws_security_token', mfa_session['Credentials']['SessionToken'])
+    parser.set('default',
+               'aws_access_key_id',
+               mfa_session['Credentials']['AccessKeyId'])
+    parser.set('default',
+               'aws_secret_access_key',
+               mfa_session['Credentials']['SecretAccessKey'])
+    parser.set('default',
+               'aws_security_token',
+               mfa_session['Credentials']['SessionToken'])
 
-    # Save the final credentials file
-    with open(os.path.join(home_dir, '.aws', 'credentials'), 'wb') as configfile:
-        parser.write(configfile)
+    _savecfg(config_file, parser)
 
-    sys.stdout.write('                    [\033[92mOK\033[0m]\n')
-    sys.stdout.write('Success. Your token expires at {} (UTC)\n'.format(parse(mfa_session['Credentials']['Expiration'])))
+    sys.stdout.write('      [\033[92mOK\033[0m]\n')
+    sys.stdout.write(
+        'Success. Your token expires at {} (UTC)\n'.format(
+            parse(
+                mfa_session['Credentials']['Expiration'])))
     sys.stdout.flush()
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='A tool to automate AWS CLI MFA')
+    parser = argparse.ArgumentParser(
+        description='A tool to automate AWS CLI MFA')
     parser.add_argument('-t', '--token', type=str, required=True,
-                        help='MFA token from device'
-                        )
+                        help='MFA token from device')
     parser.add_argument('-d', '--duration', type=int, default='43200',
-                        help='Duration of token in seconds, from 900 (15 minutes) to 129600 (36 hours)')
+                        help='Duration of token in seconds, from 900 \
+                        (15 minutes) to 129600 (36 hours)')
 
     args = parser.parse_args()
 
@@ -121,7 +149,7 @@ if __name__ == '__main__':
         sys.exit(1)
 
     if args.duration < 900 or args.duration > 129600:
-        sys.stderr.write('Error: Duration must be between 900 and 129000 seconds\n')
+        sys.stderr.write('Err: Duration must be from 900 to 129000 seconds\n')
         sys.exit(1)
 
     _getcreds(args.token, args.duration)
